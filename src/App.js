826fdef4fda540e187aa34107562b9875cdf7e09
Nouman Ali Zaidi <noumanali22222@gmail.com>
2023-05-23 23:09:21 -0700

    Basic structre of the app on which we work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <Navbar />
+        <News />
       </div>
     )
   }
 }
-
-
