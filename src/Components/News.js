b417156d7df9ff693f523ffcca6252d68aefa019
Nouman Ali Zaidi <noumanali22222@gmail.com>
2023-05-24 09:21:40 -0700

    fetching data form newsapi.org and then adding next and previous buttons to populate NewsItems

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,11 +2,77 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 
 export class News extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            articles: [],
+            loading: false,
+            page: 1
+        }
+    }
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=606aacaa1da640daa9dfbf280305b3b9";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState(
+            {
+                articles: parsedData.articles,
+                totalResults: parsedData.totalResults
+            }
+        );
+    }
+    handleNextClick = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        } else {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=606aacaa1da640daa9dfbf280305b3b9&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            console.log(parsedData);
+            this.setState(
+                {
+                    articles: parsedData.articles,
+                    page: this.state.page + 1
+                }
+            )
+        }
+    }
+    handlePreviousClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=606aacaa1da640daa9dfbf280305b3b9&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState(
+            {
+                articles: parsedData.articles,
+                page: this.state.page - 1
+            }
+        )
+    }
     render() {
         return (
-            <div>
-                This is a news Component
-                <NewsItem />
+            <div className='container my-3'>
+                <h2>NewsMonkey - Top Headlines</h2>
+                <div className="row">
+                    {this.state.articles.map((element) => {
+                        return (
+                            <div className="col-md-4" key={element.url}>
+                                <NewsItem
+                                    title={element.title ? element.title : "No Title"}
+                                    description={element.description ? element.description : "No description provided"}
+                                    url={element.urlToImage}
+                                    newsUrl={element.url}
+                                />
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type='button' className='btn btn-dark' onClick={this.handlePreviousClick}>&laquo; Previous</button>
+                    <button type='button' className='btn btn-dark' onClick={this.handleNextClick}>Next &raquo;</button>
+                </div>
             </div>
         )
     }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
+        let { title, description, url, newsUrl } = this.props;
         return (
-            <div>
-                This is the news Item
+            <div className="card my-3" style={{ width: "18rem" }}>
+                <img src={url ? url : "https://img.jagranjosh.com/images/2023/May/2352023/Inouye-Solar-Telescope-What-is-Inouye-Solar-Telescope's-New-Images-min.jpg"} className="card-img-top" alt="https://img.jagranjosh.com/images/2023/May/2352023/Inouye-Solar-Telescope-What-is-Inouye-Solar-Telescope's-New-Images-min.jpg" />
+                <div className="card-body">
+                    <h5 className="card-title">{title}...</h5>
+                    <p className="card-text">{description}...</p>
+                    <a href={newsUrl} target='-blank' className="btn btn-dark btn-sm">Read More</a>
+                </div>
             </div>
         )
     }
