c3e7b5ed5ce0cc761622e554032acd01a356d3a8
Nouman Ali Zaidi <noumanali22222@gmail.com>
2023-05-27 03:03:12 -0700

    changing class based components to function based components and adding sticky Navbar.

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-    render() {
-        let { title, description, url, newsUrl, date, author, source } = this.props;
-        return (
-            <div className="card my-3">
-                <div className="card">
-                    <div className='d-flex justify-content-end position-absolute ' style={{ right: 0 }}>
-                        <span className=' badge rounded-pill bg-danger'>{source}</span>
-                    </div>
-                    <img src={url ? url : "https://img.jagranjosh.com/images/2023/May/2352023/Inouye-Solar-Telescope-What-is-Inouye-Solar-Telescope's-New-Images-min.jpg"} className="card-img-top" alt="https://img.jagranjosh.com/images/2023/May/2352023/Inouye-Solar-Telescope-What-is-Inouye-Solar-Telescope's-New-Images-min.jpg" />
-                    <div className="card-body">
-                        <h5 className="card-title">{title} </h5>
-                        <p className="card-text">{description}</p>
-                        <p className='card-text'><small className='text-muted'>By {author ? author : "Unknown"} on {new Date(date).toDateString()}</small></p>
-                        <a href={newsUrl} target='-blank' className="btn btn-dark btn-sm">Read More</a>
-                    </div>
+const NewsItem = (props) => {
+    let { title, description, url, newsUrl, date, author, source } = props;
+    return (
+        <div className="card my-3">
+            <div className="card">
+                <div className='d-flex justify-content-end position-absolute ' style={{ right: 0 }}>
+                    <span className=' badge rounded-pill bg-danger'>{source}</span>
+                </div>
+                <img src={url ? url : "https://img.jagranjosh.com/images/2023/May/2352023/Inouye-Solar-Telescope-What-is-Inouye-Solar-Telescope's-New-Images-min.jpg"} className="card-img-top" alt="https://img.jagranjosh.com/images/2023/May/2352023/Inouye-Solar-Telescope-What-is-Inouye-Solar-Telescope's-New-Images-min.jpg" />
+                <div className="card-body">
+                    <h5 className="card-title">{title} </h5>
+                    <p className="card-text">{description}</p>
+                    <p className='card-text'><small className='text-muted'>By {author ? author : "Unknown"} on {new Date(date).toDateString()}</small></p>
+                    <a href={newsUrl} target='-blank' className="btn btn-dark btn-sm">Read More</a>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default NewsItem
--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import loading from "../loading.gif";
 
-export class Spinner extends Component {
-    render() {
-        return (
-            <div className='text-center'>
-                <img style={{ width: "5%", objectFit: "contain" }} src={loading} alt="Loading" />
-            </div>
-        )
-    }
+const Spinner = () => {
+
+    return (
+        <div className='text-center'>
+            <img style={{ width: "5%", objectFit: "contain" }} src={loading} alt="Loading" />
+        </div>
+    )
 }
 
 export default Spinner
